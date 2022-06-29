d73cf69f3ad28acc25d287aa5add702dd3fa1c95
Indrajith A C <indrajithac@gmail.com>
2022-06-29 23:40:42 +0530

    post design  completed

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import './Posts.css'
 
 function Posts() {
-  return (
-    <div>Posts</div>
-  )
+    return (
+        <>
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img
+                            className="postProfileImg"
+                            src="/assets/dp.jpeg"
+                            alt=""
+                        />
+                        <span className="postUsername">
+                            Username
+                        </span>
+                        <span className="postDate"> 5 min ago</span>
+                    </div>
+                    
+                </div>
+                <div className="postCenter">
+                    <span className="postText">Message</span>
+                    <img className="postImg" src="/assets/dp.jpeg" alt="" />
+                </div>
+
+            </div>
+        </div>
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img
+                            className="postProfileImg"
+                            src="/assets/dp.jpeg"
+                            alt=""
+                        />
+                        <span className="postUsername">
+                            Username
+                        </span>
+                        <span className="postDate"> 5 min ago</span>
+                    </div>
+                    
+                </div>
+                <div className="postCenter">
+                    <span className="postText">Message</span>
+                    <img className="postImg" src="/assets/dp.jpeg" alt="" />
+                </div>
+
+            </div>
+        </div>
+        </>
+        
+    )
 }
 
 export default Posts
