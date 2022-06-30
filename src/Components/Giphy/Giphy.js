b6d8493b01ffda7098350d43e20dfc0f50e806aa
Indrajith A C <indrajithac@gmail.com>
2022-06-30 12:05:54 +0530

    gif search working properly

--- src/Components/Giphy/Giphy.js
+++ src/Components/Giphy/Giphy.js
@@ -7,32 +7,51 @@ import axios from 'axios';
 
 function Giphy() {
     const [gifs, setGifs] = useState([])
+    const [gifSearch, setGifSearch] = useState("")
 
     //for fetching gifs from giphy.com
-    useEffect(() => {
-        const fetchData = async () => {
-            const results = await axios("https://api.giphy.com/v1/gifs/trending", {
-                params: {
-                    api_key: "Kx0T9kk2dQCRk03BGGM5OVBDclmSOIEl",
-                    limit: 100
-                }
-            });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const results = await axios("https://api.giphy.com/v1/gifs/search", {
+            params: {
+                api_key: "Kx0T9kk2dQCRk03BGGM5OVBDclmSOIEl",
+                q: gifSearch,
+                limit: 15
+            }
+        });
 
-            console.log(results.data.data);
-            setGifs(results.data.data);
-        }
-        fetchData();
-    }, [])
-    console.log(gifs);
+        console.log(results.data.data);
+        setGifs(results.data.data);
+
+
+    }
+   
+  
+
+    const rendergifs = () => {
+        return gifs.map(gif => {
+            return (
+                <div key={gif.id} className='gifImg' >
+                    <img src={gif.images.fixed_width.url} />
+                </div>
+            );
+        });
+    }
+
     return (
         <div className='gifContainer'>
             <div className='gifSearchBar'>
-                <SearchIcon className='searchIcon' />
-                <input className='searchInput' placeholder='Search gifs...' />
+                <input className='searchInput' placeholder='Search gifs...' onChange={(e) => setGifSearch(e.target.value)} />
+                <button onClick={handleSubmit} className="searchButton">
+                    <SearchIcon className='searchIcon' />
+                </button>
             </div>
             <div>
-              
+                <div className='renderGifs'>
+                    {rendergifs()}
+                </div>
             </div>
+
         </div>
     )
 }
